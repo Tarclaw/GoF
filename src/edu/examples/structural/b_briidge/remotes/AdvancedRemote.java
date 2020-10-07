df595f83a93e361631ba26ad372b03a8ddccc218
Andriy Paliychuk <a.b.paliychuk@gmail.com>
2020-10-07 20:29:08 +0300

    example of bridge

---
+++ src/edu/examples/structural/b_briidge/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package edu.examples.structural.b_briidge.remotes;
+
+import edu.examples.structural.b_briidge.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ src/edu/examples/structural/b_briidge/remotes/Remote.java
@@ -0,0 +1,15 @@
+package edu.examples.structural.b_briidge.remotes;
+
+public interface Remote {
+
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+
+}
