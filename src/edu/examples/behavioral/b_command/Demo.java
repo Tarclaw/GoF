d09bf818a9ee47733aa2bbe67151af1e277c8060
Andriy Paliychuk <a.b.paliychuk@gmail.com>
2020-10-08 11:56:40 +0300

    example of command

---
+++ src/edu/examples/behavioral/b_command/Demo.java
@@ -0,0 +1,10 @@
+package edu.examples.behavioral.b_command;
+
+import edu.examples.behavioral.b_command.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
