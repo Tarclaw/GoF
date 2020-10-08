64fcd64cb7b5c02071a019583751d39ed31547d6
Andriy Paliychuk <a.b.paliychuk@gmail.com>
2020-10-08 15:34:38 +0300

    example of memento

---
+++ src/edu/examples/behavioral/e_memento/commands/Command.java
@@ -0,0 +1,6 @@
+package edu.examples.behavioral.e_memento.commands;
+
+public interface Command {
+    String getName();
+    void execute();
+}
---
+++ src/edu/examples/behavioral/e_memento/histories/Memento.java
@@ -0,0 +1,17 @@
+package edu.examples.behavioral.e_memento.histories;
+
+import edu.examples.behavioral.e_memento.editors.Editor;
+
+public class Memento {
+    private String backup;
+    private Editor editor;
+
+    public Memento(Editor editor) {
+        this.editor = editor;
+        this.backup = editor.backup();
+    }
+
+    public void restore() {
+        editor.restore(backup);
+    }
+}
